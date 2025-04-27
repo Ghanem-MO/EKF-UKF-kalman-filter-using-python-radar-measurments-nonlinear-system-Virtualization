4d6da70e13af595f24038efcc01797d155e88f53
Ghanem <112939723+Ghanem-MO@users.noreply.github.com>
2025-04-27 16:37:12 +0300

    Update EKF-nonlinear-system.py

--- EKF-nonlinear-system.py
+++ EKF-nonlinear-system.py
@@ -1,10 +1,3 @@
-"""
-    The code simulates a scenario where a "radar" 
-    observes an object moving in two dimensions (X, Y) with a constant velocity,
-    while there is noise in the measurements.
-    Then, it uses an Extended Kalman Filter (EKF)
-    to accurately estimate the object's position from the noisy measurements.
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
